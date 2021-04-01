bad19a66f04535435964c7e86d7b960ed0ea448c
Sergio Valdez <sergioavr93@gmail.com>
2021-04-01 14:02:19 -0600

    Archivo para configuración local

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import local_settings as ls
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://ieeu:ieeup@localhost/iee"
-
+SQLALCHEMY_DATABASE_URL = f'postgresql://{ls.user}:{ls.password}@{ls.host}/{ls.database}'
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     SQLALCHEMY_DATABASE_URL
